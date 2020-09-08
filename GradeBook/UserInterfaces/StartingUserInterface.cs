fcd45b86990ca78d0af8fd9ccce44773b2439ff2
DomScott <dom.m.scott@gmail.com>
2020-09-08 15:03:41 +1000

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -43,21 +43,21 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            BaseGradeBook gradebook;
+            BaseGradeBook gradeBook;
             if (type == "standard")
             {
-                var gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name);
             }
             else if (type == "ranked")
             {
-                var gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name);
             }
             else
             {
                 Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
-                return;
+                return; 
             }
-
+            
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
