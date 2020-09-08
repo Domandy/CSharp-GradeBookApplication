b1154dd966c398cf5344c7a951cf433fe98e7b53
DomScott <dom.m.scott@gmail.com>
2020-09-08 17:39:08 +1000

    Update BaseGradeBook.cs

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -114,6 +114,7 @@ namespace GradeBook.GradeBooks
             if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
             {
                 gpa++;
+                return gpa;
             }
             switch (letterGrade)
             {
